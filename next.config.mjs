cdc6d3bf0ebb117723cbbbbda3ebf17db81c31ce
hiromps <akihiro19970324@gmail.com>
2024-08-27 17:39:10 +0900

    Add admin dashboard features

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,9 @@
 const nextConfig = {};
 
 export default nextConfig;
+module.exports = {
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      // カスタムWebpack設定をここに記述
+      return config
+    },
+  }
